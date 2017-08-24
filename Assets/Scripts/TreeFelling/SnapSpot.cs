35f3866dc8876306a0431d0651f228d0395cb4dc
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2017-08-24 00:32:24 -0700

    Adding locking behavior around trees in TreeFelling

---
+++ Assets/Scripts/TreeFelling/SnapSpot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapSpot : MonoBehaviour 
+{
+	public TreeBehavior parentTreeBehavior;
+
+	void OnStart()
+	{
+		// parentTreeBehavior = transform.parent.GetComponent<TreeBehavior>();
+	}
+
+	void OnTriggerEnter()
+	{
+		parentTreeBehavior.SetSnapInfo(transform, true);
+	}
+
+	void OnTriggerExit()
+	{
+		parentTreeBehavior.SetSnapInfo(false);
+	}
+}
---
+++ Assets/Scripts/TreeFelling/TreeBehavior.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class TreeBehavior : MonoBehaviour 
+{
+	
+	private Transform snapLocation;
+	private bool playerIsLocked;
+	private bool canSnapPlayer;
+
+	GameObject playerObj;
+	private Transform playerTransform;
+	private ThirdPersonUserControl userControl;
+
+	private RigidbodyConstraints startingConstraints;
+
+	void Start () 
+	{
+		
+		playerObj = GameObject.FindGameObjectWithTag("Player");
+
+		playerTransform = playerObj.transform;
+		userControl = playerObj.GetComponent<ThirdPersonUserControl>();
+
+		startingConstraints = RigidbodyConstraints.FreezeRotation;
+	}
+	
+	void Update () 
+	{
+		if (canSnapPlayer && Input.GetButtonDown("Interact"))
+		{
+			if (!playerIsLocked)
+			{
+				SnapPlayer();
+			}
+			else
+			{
+				UnlockPlayer();
+			}
+		}
+
+		if (playerIsLocked && Input.GetKeyDown(KeyCode.R))
+		{
+			RotateAroundTree();
+		}
+		
+		
+	}
+
+	public void SetSnapInfo(bool canSnap) { canSnapPlayer = canSnap; }
+
+	public void SetSnapInfo(Transform location, bool canSnap)
+	{
+		snapLocation = location;
+		canSnapPlayer = canSnap;
+	}
+
+	void SnapPlayer()
+	{
+		playerTransform.position = snapLocation.position;
+		playerTransform.rotation = snapLocation.rotation;
+
+		userControl.enabled = false;
+		playerObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+		playerIsLocked = true;
+	}
+
+	void RotateAroundTree()
+	{
+		if (playerTransform.eulerAngles.y == 0) 
+		{
+			playerTransform.position = new Vector3(playerTransform.position.x - 2, 0 , playerTransform.position.z);playerTransform.eulerAngles = new Vector3(0, 180, 0);
+		}
+		else if (playerTransform.eulerAngles.y == 90)
+		{
+			playerTransform.position = new Vector3(playerTransform.position.x, 0 , playerTransform.position.z + 2);
+			playerTransform.eulerAngles = new Vector3(0, 270, 0);
+		}
+		else if (playerTransform.eulerAngles.y == 180)
+		{
+			playerTransform.position = new Vector3(playerTransform.position.x + 2, 0 , playerTransform.position.z);
+			playerTransform.eulerAngles = new Vector3(0, 0, 0);
+		}
+		else if (playerTransform.eulerAngles.y == 270)
+		{
+			playerTransform.position = new Vector3(playerTransform.position.x, 0 , playerTransform.position.z - 2);
+			playerTransform.eulerAngles = new Vector3(0, 90, 0);
+		}
+
+		
+		Debug.Log("World + Local:" + (playerTransform.position + snapLocation.localPosition));
+		Debug.Log("World - Local:" + (playerTransform.position - snapLocation.localPosition));
+	}
+
+	void UnlockPlayer()
+	{
+		userControl.enabled = true;
+		playerObj.GetComponent<Rigidbody>().constraints = startingConstraints;
+
+		playerIsLocked = false;
+	}
+}
