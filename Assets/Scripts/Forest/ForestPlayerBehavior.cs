0b562e8850e275392f47376bacf4ac953333461c
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2017-10-07 16:23:45 -0700

    Created & added Logger prefab to scenes

--- Assets/Scripts/Forest/ForestPlayerBehavior.cs
+++ Assets/Scripts/Forest/ForestPlayerBehavior.cs
@@ -12,8 +12,6 @@ namespace Forest
 	{
 		public static ForestPlayerBehavior PlayerBehaviorReference;
 
-		// private ThirdPersonUserControl userControl;
-
 		private RigidbodyConstraints startingConstraints;
 
 		private bool playerIsLocked;
@@ -32,15 +30,11 @@ namespace Forest
 		void Start () 
 		{
 			PlayerBehaviorReference = this;
-
-			// userControl = GetComponent<ThirdPersonUserControl>();
 			startingConstraints = RigidbodyConstraints.FreezeRotation;
 		}
 		
 		void Update () 
 		{
-			DebugPanel.Log("Euler Y: ", transform.eulerAngles.y);
-
 			if (Input.GetButtonDown("Interact") && canSnapPlayer && !forestTreeToCut.HasFallen())
 			{
 				if (!playerIsLocked)
@@ -57,7 +51,6 @@ namespace Forest
 			{
 				if (Input.GetKeyDown(KeyCode.R) || Input.GetButtonDown("Right Bumper") || Input.GetButtonDown("Left Bumper"))
 				{
-					Debug.Log("Rotate Call");
 					RotateAroundTree();
 				}
 
@@ -103,7 +96,6 @@ namespace Forest
 			transform.position = snapLocation.position;
 			transform.rotation = snapLocation.rotation;
 
-			// userControl.enabled = false;
 			GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
 			inForwardPosition = true;
@@ -118,7 +110,6 @@ namespace Forest
 			CharacterInputController.SetCanTurn(true);
 			CharacterInputController.InitiateLoggingState(AnimState.NONE);
 
-			// userControl.enabled = true;
 			GetComponent<Rigidbody>().constraints = startingConstraints;
 			
 			inForwardPosition = true;
@@ -129,36 +120,29 @@ namespace Forest
 
 		void RotateAroundTree()
 		{
-			Debug.Log("Euler: " + transform.eulerAngles.y);
-			float eulerValY = transform.eulerAngles.y;
-
 			if (Mathf.Approximately(transform.eulerAngles.y, 30f)) 
 			{
-				Debug.Log("30");
 				transform.position = new Vector3(transform.position.x, 0 , transform.position.z + 2);
 				transform.eulerAngles = new Vector3(0, 210, 0);
 			}
 			else if (Mathf.Approximately(transform.eulerAngles.y, 120f)) 
 			{
-				Debug.Log("120");
 				transform.position = new Vector3(transform.position.x + 2, 0 , transform.position.z);
 				transform.eulerAngles = new Vector3(0, 300, 0);
 			}
 			else if (Mathf.Approximately(transform.eulerAngles.y, 210f)) 
 			{
-				Debug.Log("210");
 				transform.position = new Vector3(transform.position.x, 0 , transform.position.z - 2);
 				transform.eulerAngles = new Vector3(0, 30, 0);
 			}
 			else if (Mathf.Approximately(transform.eulerAngles.y, 300f)) 
 			{
-				Debug.Log("300");
 				transform.position = new Vector3(transform.position.x - 2, 0 , transform.position.z);
 				transform.eulerAngles = new Vector3(0, 120, 0);
 			}
 
-			inForwardPosition = true;
-			inBackwardPosition = false;
+			// inForwardPosition = true;
+			// inBackwardPosition = false;
 		}
 
 		
