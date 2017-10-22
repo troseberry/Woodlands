afc52366f797eb1c71c64ae94564c3d118e46f89
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2017-10-21 18:35:06 -0700

    Update Firewood Splitting activity
    -now visually shows logs getting split
    - added physics/rigidbodies to apply force to throw log pieces after
    being chopped

--- Assets/Scripts/LoggingActivities/FirewoodSplitting/LogBehavior.cs
+++ Assets/Scripts/LoggingActivities/FirewoodSplitting/LogBehavior.cs
@@ -9,6 +9,8 @@ namespace FirewoodSplitting
 		private QualityGrade qualityGrade;
 		private LogPileBehavior associatedLogPile;
 
+		public Rigidbody[] firewoodPieces;
+
 		private bool hasBeenSplit = false;
 
 		void Start () 
@@ -55,7 +57,22 @@ namespace FirewoodSplitting
 
 		void ApplySplitForce()
 		{
+			// Vector3 randomLeft = new Vector3(firewoodPieces[0].transform.position.x, 
+			// 								Random.Range(firewoodPieces[0].transform.position.y + 0.25f, firewoodPieces[0].transform.position.y - 0.25f), 
+			// 								Random.Range(firewoodPieces[0].transform.position.z + 0.125f, firewoodPieces[0].transform.position.z - 0.125f));
+											
+			// Vector3 randomRight = new Vector3(firewoodPieces[1].transform.position.x, 
+			// 								Random.Range(firewoodPieces[0].transform.position.y + 0.25f, firewoodPieces[0].transform.position.y - 0.25f), 
+			// 								Random.Range(firewoodPieces[0].transform.position.z + 0.125f, firewoodPieces[0].transform.position.z - 0.125f));
 
+			firewoodPieces[0].constraints = RigidbodyConstraints.None;
+			firewoodPieces[1].constraints = RigidbodyConstraints.None;
+
+			// firewoodPieces[0].AddForceAtPosition(transform.right * -3, randomLeft, ForceMode.Impulse);
+			// firewoodPieces[1].AddForceAtPosition(transform.right * 3, randomRight, ForceMode.Impulse);
+
+			firewoodPieces[0].AddForce(transform.right * -3, ForceMode.Impulse);
+			firewoodPieces[1].AddForce(transform.right * 3, ForceMode.Impulse);
 		}
 
 		void PhaseOutFirewood()
@@ -75,6 +92,16 @@ namespace FirewoodSplitting
 		{
 			hasBeenSplit = false;
 			associatedLogPile.UpdateLogPile();
+
+			firewoodPieces[0].constraints = RigidbodyConstraints.FreezeAll;
+			firewoodPieces[1].constraints = RigidbodyConstraints.FreezeAll;
+
+			firewoodPieces[0].transform.localPosition = new Vector3(-0.0625f, 0, 0);
+			firewoodPieces[1].transform.localPosition = new Vector3(0.0625f, 0, 0);
+
+			firewoodPieces[0].transform.eulerAngles = new Vector3(-180, 180, 0);
+			firewoodPieces[1].transform.eulerAngles = new Vector3(-180, 180, 0);
+
 			gameObject.SetActive(true);
 		}
 	}
