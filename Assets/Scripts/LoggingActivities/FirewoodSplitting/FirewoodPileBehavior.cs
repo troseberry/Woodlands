9482e2f9d4174c3a3eb568f10b0e781285cd238d
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2017-10-22 23:52:01 -0700

    Added firewood pile to homestead
    - firewood pile that updates based on total firewood count
    (non-interactable)

---
+++ Assets/Scripts/LoggingActivities/FirewoodSplitting/FirewoodPileBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FirewoodSplitting
+{
+	public class FirewoodPileBehavior : MonoBehaviour 
+	{
+		void Start () 
+		{
+			UpdateFirewoodPile();
+		}
+		
+		public void UpdateFirewoodPile()
+		{
+			int[] firewoodArray = HomesteadStockpile.GetAllFirewoodCount();
+			int totalFirewod = 0;
+			for (int i = 0; i < firewoodArray.Length; i++)
+			{
+				totalFirewod += firewoodArray[i];
+			}
+
+			transform.GetChild(0).gameObject.SetActive(totalFirewod > 0);
+			transform.GetChild(1).gameObject.SetActive(totalFirewod > 10);
+			transform.GetChild(2).gameObject.SetActive(totalFirewod > 25);
+			transform.GetChild(3).gameObject.SetActive(totalFirewod > 50);
+			transform.GetChild(4).gameObject.SetActive(totalFirewod > 100);
+		}
+	}
+}
