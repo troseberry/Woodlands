7f75f51a633a9482db1f0eed4d1ed8fe081aa0f0
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2017-12-11 16:27:42 -0800

    Disabled ability to turn in expired contracts

--- Assets/Scripts/UI/MailboxCanvas.cs
+++ Assets/Scripts/UI/MailboxCanvas.cs
@@ -71,6 +71,8 @@ public class MailboxCanvas : MonoBehaviour
 
 			contractsContent.GetChild(j).GetChild(6).GetComponent<Button>().interactable = activeContracts[j].CanBeCompleted();
 			contractsContent.GetChild(j).GetChild(6).GetChild(0).GetComponent<Text>().text = "Turn In";
+
+			contractsContent.GetChild(j).GetChild(7).gameObject.SetActive(activeContracts[j].IsExpired());
 		}
 	}
 
